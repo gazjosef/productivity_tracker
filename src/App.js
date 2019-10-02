f584d0a4e1a6b75e676a6c9f181d0ef71eecb79f
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-10-02 21:57:43 +1000

    Added Calendar UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+import Calendar from "./Layout/Calendar";
+
+const style = {
+  position: "relative",
+  margin: "50px auto"
+};
+
+class App extends Component {
+  onDayClick = (e, day) => {
+    alert(day);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          style={style}
+          width="302px"
+          onDayClick={(e, day) => this.onDayClick(e, day)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
