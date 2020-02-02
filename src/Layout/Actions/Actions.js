1d657607017a5f140190dd537e10780779e8f1cb
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-02-02 19:12:28 +1100

    Added Actions.js & ProgressBar.js / Modified Home.js

---
+++ src/Layout/Actions/Actions.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from "react";
+
+const Actions = () => {
+  return (
+    <Fragment>
+      <section id="actions" className="py-4 mb-4 bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <a
+                href="#"
+                className="btn btn-success btn-block"
+                data-toggle="modal"
+                data-target="#addPostModal"
+              >
+                <i className="fas fa-plus"></i> Add Positive
+              </a>
+            </div>
+            <div className="col-md-3">
+              <a
+                href="#"
+                className="btn btn-danger btn-block"
+                data-toggle="modal"
+                data-target="#addCategoryModal"
+              >
+                <i className="fas fa-plus"></i> Add Negative
+              </a>
+            </div>
+            <div className="col-md-3">
+              <a
+                href="#"
+                className="btn btn-warning btn-block"
+                data-toggle="modal"
+                data-target="#addUserModal"
+              >
+                <i className="fas fa-plus"></i> Add Neutral
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Actions;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 
 // Layout
+import Actions from "../../Layout/Actions/Actions";
 import Calendar from "../../Layout/Calendar/Calendar";
+import ProgressBar from "../../Layout/ProgressBar/ProgressBar";
 import Sidebar from "../../Layout/Sidebar/Sidebar";
 
 const Home = () => {
@@ -18,96 +20,13 @@ const Home = () => {
           </div>
         </div>
       </header>
-      <section id="actions" className="py-4 mb-4 bg-light">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3">
-              <a
-                href="#"
-                className="btn btn-success btn-block"
-                data-toggle="modal"
-                data-target="#addPostModal"
-              >
-                <i className="fas fa-plus"></i> Add Positive
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a
-                href="#"
-                className="btn btn-danger btn-block"
-                data-toggle="modal"
-                data-target="#addCategoryModal"
-              >
-                <i className="fas fa-plus"></i> Add Negative
-              </a>
-            </div>
-            <div className="col-md-3">
-              <a
-                href="#"
-                className="btn btn-warning btn-block"
-                data-toggle="modal"
-                data-target="#addUserModal"
-              >
-                <i className="fas fa-plus"></i> Add Neutral
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Actions />
       <section id="track">
         <div className="container">
           <div className="row">
             <div className="col-md-9">
-              {/* <!-- CALENDAR --> */}
               <Calendar />
-              {/* <!-- TRACKER --> */}
-              <div className="card">
-                <div className="card-header">
-                  <h4>Latest Habits</h4>
-                </div>
-                <div className="card-body">
-                  <div className="progress mb-3" style={{ height: "50px" }}>
-                    <div
-                      className="progress-bar progress-bar-striped bg-success"
-                      role="progressbar"
-                      style={{ width: "25%" }}
-                      aria-valuenow="25"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <div className="progress mb-3" style={{ height: "50px" }}>
-                    <div
-                      className="progress-bar progress-bar-striped bg-info"
-                      role="progressbar"
-                      style={{ width: "50%" }}
-                      aria-valuenow="50"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <div className="progress mb-3" style={{ height: "50px" }}>
-                    <div
-                      className="progress-bar progress-bar-striped bg-warning"
-                      role="progressbar"
-                      style={{ width: "75%" }}
-                      aria-valuenow="75"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <div className="progress" style={{ height: "50px" }}>
-                    <div
-                      className="progress-bar bg-danger"
-                      role="progressbar"
-                      style={{ width: "100%" }}
-                      aria-valuenow="100"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                </div>
-              </div>
+              <ProgressBar />
             </div>
             <div className="col-md-3">
               <Sidebar />
