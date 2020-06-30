ec1b139f783540af4db1220c52477c0a38f149c3
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-30 22:49:36 +1000

    Working Chart

--- src/Layout/DailyPerformance/chart.js
+++ src/Layout/DailyPerformance/chart.js
@@ -2,8 +2,46 @@ import React, { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 
 class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [
+          'Sydney',
+          'Melbourne',
+          'Brisbane',
+          'Perth',
+          'Adelaide',
+          'Gold Coast',
+        ],
+        datasets: [
+          {
+            label: 'Population',
+            data: [4627345, 4246375, 2189878, 1896548, 1225235, 591473],
+            // backgroundColor: 'green',
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)',
+            ],
+          },
+        ],
+      },
+    };
+  }
   render() {
-    return <div className="chart">CHART COMPONENT</div>;
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+    );
   }
 }
 
