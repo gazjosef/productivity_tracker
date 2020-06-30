6423eb7dda0a2b35f8415394ac76511f841a8082
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-30 23:09:45 +1000

    Updated Chart State

--- src/Layout/DailyPerformance/DailyPerformance.js
+++ src/Layout/DailyPerformance/DailyPerformance.js
@@ -2,6 +2,44 @@ import React, { Component, Fragment } from 'react';
 import Chart from './Chart';
 
 class DailyPerformance extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {},
+    };
+  }
+
+  getChartData() {
+    // AJAX Calls Here
+    this.setState({
+      chartData: {
+        labels: [
+          'Sydney',
+          'Melbourne',
+          'Brisbane',
+          'Perth',
+          'Adelaide',
+          'Gold Coast',
+        ],
+        datasets: [
+          {
+            label: 'Population',
+            data: [4627345, 4246375, 2189878, 1896548, 1225235, 591473],
+            // backgroundColor: 'green',
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)',
+            ],
+          },
+        ],
+      },
+    });
+  }
   render() {
     return (
       <Fragment>
