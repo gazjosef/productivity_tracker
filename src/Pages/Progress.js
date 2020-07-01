07c20ca7539741af072f75004f0aaad2edb77c35
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-01 13:40:31 +1000

    Updated new Progress.js

--- src/Pages/Progress.js
+++ src/Pages/Progress.js
@@ -1,11 +1,50 @@
 import React, { Component, Fragment } from 'react';
 
+// LAYOUT
+import DailyPerformanceChart from '../Layout/DailyPerformance/Chart';
+
 export default class Progress extends Component {
   constructor(props) {
     super(props);
     this.state = {
       chartData: props.chartData,
     };
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    // AJAX Calls Here
+    this.setState({
+      chartData: {
+        labels: [
+          'Sydney',
+          'Melbourne',
+          'Brisbane',
+          'Perth',
+          'Adelaide',
+          'Gold Coast',
+        ],
+        datasets: [
+          {
+            label: 'Population',
+            data: [4627345, 4246375, 2189878, 1896548, 1225235, 591473],
+            // backgroundColor: 'green',
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)',
+            ],
+          },
+        ],
+      },
+    });
   }
   render() {
     return (
@@ -24,12 +63,16 @@ export default class Progress extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-9">
-              <ProgressBar />
+              {/* <ProgressBar />
               <ThisWeek />
               <CompletionRate />
-              <CheckInTime />
-              <DailyPerformance />
-              <YearlyPerformance />
+              <CheckInTime /> */}
+              <DailyPerformanceChart
+                chartData={this.state.chartData}
+                location="Australia"
+                legendPosition="bottom"
+              />
+              {/* <YearlyPerformance /> */}
             </div>
           </div>
         </div>
