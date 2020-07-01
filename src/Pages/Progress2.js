2945f8418dfb87ca58f68b48c3c2e3efcbc2da65
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-01 18:15:19 +1000

    Created Progress2.js

--- src/Pages/Progress2.js
+++ src/Pages/Progress2.js
@@ -63,16 +63,48 @@ export default class Progress extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-9">
-              {/* <ProgressBar />
-              <ThisWeek />
-              <CompletionRate />
-              <CheckInTime /> */}
-              <DailyPerformanceChart
-                chartData={this.state.chartData}
-                location="Australia"
-                legendPosition="bottom"
-              />
-              {/* <YearlyPerformance /> */}
+              <div className="card mt-3">
+                <div className="card-header">
+                  <h4>Latest Habits</h4>
+                </div>
+                <div className="card-body"></div>
+              </div>
+              <div className="card mt-3">
+                <div className="card-header">
+                  <h4>This Week</h4>
+                </div>
+                <div className="card-body"></div>
+              </div>
+              <div className="card mt-3">
+                <div className="card-header">
+                  <h4>Completion Rates</h4>
+                </div>
+                <div className="card-body"></div>
+              </div>
+              <div className="card mt-3">
+                <div className="card-header">
+                  <h4>Check-In Time</h4>
+                </div>
+                <div className="card-body"></div>
+              </div>
+              <div className="card mt-3">
+                <div className="card-header">
+                  <h4>Daily Performance</h4>
+                </div>
+                <div className="card-body">
+                  <DailyPerformanceChart
+                    chartData={this.state.chartData}
+                    location="Australia"
+                    legendPosition="bottom"
+                  />
+                </div>
+              </div>
+              <div className="card mt-3">
+                <div className="card-header">
+                  <h4>Yearly Performance</h4>
+                </div>
+                <div className="card-body"></div>
+              </div>
             </div>
           </div>
         </div>
